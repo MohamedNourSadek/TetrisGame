f90324e56106399f22b5bcac46029da3335d93e8
Mohamed Nour <71439813+MohamedNourSadek@users.noreply.github.com>
2023-02-18 17:59:22 +0200

    First Commit

---
+++ Source/Tetris/TetrisGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TetrisGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TETRIS_API ATetrisGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
